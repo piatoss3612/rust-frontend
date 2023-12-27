8975fea025ea44a31cfeae3ae5f0dead263dfaee
piatoss <crewe1746@naver.com>
2023-12-27 17:19:06 +0900

    Add logging to console using gloo

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
+use gloo::console::log;
+use serde::{Deserialize, Serialize};
 use yew::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct MyObject {
+    username: String,
+    favorite_language: String,
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let name = "Bob";
+    let my_object = MyObject {
+        username: "Bob".to_string(),
+        favorite_language: "Rust".to_string(),
+    };
+
+    log!("Hello,", name, "!");
+    log!(serde_json::to_string_pretty(&my_object).unwrap());
+
     html! {
         <div>
             <h1>{ "Hello World!!" }</h1>
