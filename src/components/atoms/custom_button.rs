d172e6e1c58ea7a302660445672402b1d7fb43e9
piatoss <crewe1746@naver.com>
2023-12-30 21:25:03 +0900

    complex useState

--- src/components/atoms/custom_button.rs
+++ src/components/atoms/custom_button.rs
@@ -3,11 +3,17 @@ use yew::prelude::*;
 #[derive(Properties, PartialEq)]
 pub struct Props {
     pub label: String,
+    pub onclick: Callback<()>,
 }
 
 #[function_component(CustomButton)]
 pub fn custom_button(props: &Props) -> Html {
+    let onclick = props.onclick.clone();
+    let button_onclick = Callback::from(move |_| {
+        onclick.emit(());
+    });
+
     html! {
-        <button class="custom-button">{ &props.label }</button>
+        <button class="custom-button" onclick={button_onclick}>{ &props.label }</button>
     }
 }
--- src/components/molecules/form.rs
+++ src/components/molecules/form.rs
@@ -1,20 +1,41 @@
+use std::ops::Deref;
+
 use crate::components::atoms::custom_button::CustomButton;
 use crate::components::atoms::text_input::TextInput;
 use yew::prelude::*;
 
+#[derive(Default, Debug, Clone)]
+struct Data {
+    pub username: String,
+    pub count: u32,
+}
+
 #[function_component(Form)]
 pub fn form() -> Html {
-    let username_state = use_state(|| "".to_string());
-    let cloned_username_state = username_state.clone();
+    let state = use_state(|| Data::default());
+
+    let cloned_state = state.clone();
     let username_changed = Callback::from(move |username: String| {
-        cloned_username_state.set(username);
+        cloned_state.set(Data {
+            username,
+            ..cloned_state.deref().clone()
+        })
+    });
+
+    let cloned_state = state.clone();
+    let button_changed = Callback::from(move |_| {
+        cloned_state.set(Data {
+            count: cloned_state.count + 1,
+            ..cloned_state.deref().clone()
+        })
     });
 
     html!(
-        <form>
+        <div>
             <TextInput name="username" handle_on_change={username_changed} />
-            <CustomButton label="Submit" />
-            <p>{ &*username_state} </p>
-        </form>
+            <CustomButton label="Submit" onclick={button_changed}/>
+            <p>{ &state.username} </p>
+            <p>{"Button clicked: "}{ state.count }</p>
+        </div>
     )
 }
